4517bf32feaddd70e3c508ac535d30d053b2029a
tsingsun <21997272@qq.com>
2023-06-07 18:25:54 +0800

    generate gql client code

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ adminx前端项目
 - 多语言 [i18n](https://www.i18next.com/)
 - 本地存储 [localforage](https://localforage.docschina.org/)
 - keep-alive [react-activation](https://github.com/CJY0208/react-activation/blob/HEAD/README_CN.md)
+- gql生成工具 [GraphQL Code Generator](https://the-guild.dev/graphql/codegen/docs)
 
 ## 快速开始
 ```shell
@@ -16,3 +17,11 @@ pnpm dev
 # 启动,使用proxy
 pnpm start
 ```
+
+## IDE配置
+
+本项目启用postcss,并且扩展名为`*.css`的文件,为了避免IDE报错.需要做如下配置:
+
+- JetBrains IDE:
+  - 安装插件`PostCSS` 
+  - `Settings` -> `Languages & Frameworks` -> `Stylesheets` -> `Dialects` -> project css dialect -> `PostCSS`
---
+++ script/gqlgen.ts
@@ -0,0 +1,36 @@
+import {CodegenConfig} from "@graphql-codegen/cli";
+import * as process from "process";
+
+let dotenv = require('dotenv')
+dotenv.config()
+dotenv.config({path: `.env.${process.env.NODE_ENV}`,override: true})
+dotenv.config({path: '.env.local',override: true})
+
+const schemaField = process.env.GQLGEN_SCHEMA || 'http://localhost:8080/graphql'
+const token = process.env.GQLGEN_TOKEN
+const tid = process.env.GQLGEN_TENANT_ID
+console.log('schema', schemaField)
+const config: CodegenConfig = {
+  schema: [
+    {
+      [schemaField]: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "X-Tenant-ID": `${tid}`,
+        }
+      }
+    }
+  ],
+  documents: 'src/**/*.tsx',
+  generates: {
+    'src/__generated__/': {
+      preset: 'client',
+      presetConfig: {
+        gqlTagName: 'gql',
+      }
+    }
+  },
+  ignoreNoDocuments: true,
+}
+
+export default config
