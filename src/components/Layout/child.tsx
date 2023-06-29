020d9faca1edf42c98935c161a6507ca280babbe
xhyu <xhyu@qeelyn.com>
2023-06-29 11:22:54 +0800

    子应用语言处理

--- src/components/Layout/child.tsx
+++ src/components/Layout/child.tsx
@@ -1,7 +1,16 @@
+import i18n from "@/i18n";
+import store from "@/store";
 import { Outlet } from "@ice/runtime"
+import { useEffect } from "react";
 import { AliveScope } from "react-activation"
 
 export default () => {
+  const [basisState] = store.useModel('basis');
+
+  useEffect(() => {
+    i18n.changeLanguage(basisState.locale);
+  }, [basisState.locale]);
+
   return <AliveScope>
     <Outlet />
   </AliveScope>
