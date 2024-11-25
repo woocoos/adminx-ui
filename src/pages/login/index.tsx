844c0ca5194d2bce954df34729998f99a1114c8a
xhyu <512106269@qq.com>
2024-11-25 21:40:47 +0800

    支持作为子应用处理

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,6 +10,8 @@ import ResetPassword from './components/resetPassword';
 import { useSearchParams } from 'ice';
 import { appAccess } from '@/services/adminx/app';
 import { RequestHeaderAuthorizationMode, getRequestHeaderAuthorization } from '@knockout-js/ice-urql/requestInterceptor';
+import { isInIcestark } from '@ice/stark-app';
+import { setItem } from '@/pkg/localStore';
 
 const ICE_APP_CODE = process.env.ICE_APP_CODE ?? '',
   ICE_ROUTER_BASENAME = process.env.ICE_ROUTER_BASENAME ?? '/',
@@ -50,6 +52,19 @@ export default () => {
           setIsLoginSuccess(true);
           await userDispatcher.loginAfter(result);
           message.success(t('login_success'));
+          if (isInIcestark()) {
+            // 子应用加载登录成功处理
+            setItem('token', result.accessToken);
+            setItem('refreshToken', result.refreshToken);
+            setItem('tenantId', tenantId);
+            if (result.user) {
+              setItem('user', {
+                id: result.user.id,
+                displayName: result.user.displayName,
+                avatar: result.user.avatar,
+              });
+            }
+          }
           location.replace(await urlSpm(redirect || `${ICE_ROUTER_BASENAME}/`.replaceAll('//', '/')));
         } else {
           message.error(t('login_not_app_access'));
