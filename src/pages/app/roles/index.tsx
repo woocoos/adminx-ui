5279960e601e336140fb086f08518973b129ba4e
xhyu <512106269@qq.com>
2024-12-18 20:00:47 +0800

    修复 KeepAlive

--- src/pages/app/roles/index.tsx
+++ src/pages/app/roles/index.tsx
@@ -16,7 +16,7 @@ import { delDataSource, saveDataSource } from '@/util';
 import { ItemType } from 'antd/es/menu/interface';
 
 
-export default () => {
+export const AppRoleList = () => {
   const { token } = useToken(),
     { t } = useTranslation(),
     [auth] = useAuth(),
@@ -160,100 +160,104 @@ export default () => {
 
 
   return (
-    <KeepAlive>
-      <PageContainer
-        header={{
-          title: t('app_role'),
-          style: { background: token.colorBgContainer },
-          breadcrumb: {
-            items: [
-              { title: t('system_conf') },
-              { title: <Link to={'/system/app'}>{t('app_manage')}</Link> },
-              { title: t('app_role') },
-            ],
-          },
-          children: <Alert
-            showIcon
-            message={
-              <>
-                <div key="1">{t('app_role_alert_msg')}</div>
-                <div key="2">{t('auto_authorization')}：{t('app_role_alert_msg_1')}</div>
-                <div key="3">{t('manual_authorization')}：{t('app_role_alert_msg_2')}</div>
-              </>
-            }
-          />,
+    <PageContainer
+      header={{
+        title: t('app_role'),
+        style: { background: token.colorBgContainer },
+        breadcrumb: {
+          items: [
+            { title: t('system_conf') },
+            { title: <Link to={'/system/app'}>{t('app_manage')}</Link> },
+            { title: t('app_role') },
+          ],
+        },
+        children: <Alert
+          showIcon
+          message={
+            <>
+              <div key="1">{t('app_role_alert_msg')}</div>
+              <div key="2">{t('auto_authorization')}：{t('app_role_alert_msg_1')}</div>
+              <div key="3">{t('manual_authorization')}：{t('app_role_alert_msg_2')}</div>
+            </>
+          }
+        />,
+      }}
+    >
+      <ProTable
+        actionRef={proTableRef}
+        search={{
+          searchText: `${t('query')}`,
+          resetText: `${t('reset')}`,
+          labelWidth: 'auto',
         }}
-      >
-        <ProTable
-          actionRef={proTableRef}
-          search={{
-            searchText: `${t('query')}`,
-            resetText: `${t('reset')}`,
-            labelWidth: 'auto',
-          }}
-          rowKey={'id'}
-          toolbar={{
-            title: `${t('app')}:${appInfo?.name || '-'}`,
-            actions: [
-              <Auth authKey="createAppRole">
-                <Button
-                  key="created"
-                  type="primary"
-                  onClick={() => {
-                    setModal({ open: true, title: t('create_role'), id: '', scene: 'create' });
-                  }}
-                >
-                  {t('create_role')}
-                </Button>
-              </Auth>,
-            ],
-          }}
-          scroll={{ x: 'max-content' }}
-          columns={columns}
-          dataSource={dataSource}
-          request={async (params) => {
-            const table = { data: [] as AppRole[], success: true, total: 0 },
-              info = searchParams.get('id') == appInfo?.id ? appInfo : await getApp();
-            if (info) {
-              const result = await getAppRoleList(info.id);
-              if (result) {
-                table.data = result.filter(item => {
-                  let isTrue = true;
-                  if (params.name) {
-                    isTrue = item.name.indexOf(params.name) > -1;
-                  }
-                  return isTrue;
-                });
-                table.total = table.data.length;
-              }
+        rowKey={'id'}
+        toolbar={{
+          title: `${t('app')}:${appInfo?.name || '-'}`,
+          actions: [
+            <Auth authKey="createAppRole">
+              <Button
+                key="created"
+                type="primary"
+                onClick={() => {
+                  setModal({ open: true, title: t('create_role'), id: '', scene: 'create' });
+                }}
+              >
+                {t('create_role')}
+              </Button>
+            </Auth>,
+          ],
+        }}
+        scroll={{ x: 'max-content' }}
+        columns={columns}
+        dataSource={dataSource}
+        request={async (params) => {
+          const table = { data: [] as AppRole[], success: true, total: 0 },
+            info = searchParams.get('id') == appInfo?.id ? appInfo : await getApp();
+          if (info) {
+            const result = await getAppRoleList(info.id);
+            if (result) {
+              table.data = result.filter(item => {
+                let isTrue = true;
+                if (params.name) {
+                  isTrue = item.name.indexOf(params.name) > -1;
+                }
+                return isTrue;
+              });
+              table.total = table.data.length;
             }
-            setSelectedRowKeys([]);
-            setDataSource(table.data);
-            return table;
-          }}
-          rowSelection={{
-            selectedRowKeys: selectedRowKeys,
-            onChange: (selectedRowKeys: string[]) => { setSelectedRowKeys(selectedRowKeys); },
-            type: 'checkbox',
-          }}
-        />
-        <CreateAppRole
-          x-if={modal.scene === 'create'}
-          open={modal.open}
-          title={modal.title}
-          id={modal.id}
-          appId={appInfo?.id}
-          onClose={onDrawerClose}
-        />
-        <DrawerRolePolicy
-          x-if={modal.scene === 'addPolicy' && modal.open}
-          open={modal.open}
-          title={modal.title}
-          appInfo={appInfo}
-          roleInfo={modal.roleInfo}
-          onClose={onDrawerClose}
-        />
-      </PageContainer>
-    </KeepAlive>
+          }
+          setSelectedRowKeys([]);
+          setDataSource(table.data);
+          return table;
+        }}
+        rowSelection={{
+          selectedRowKeys: selectedRowKeys,
+          onChange: (selectedRowKeys: string[]) => { setSelectedRowKeys(selectedRowKeys); },
+          type: 'checkbox',
+        }}
+      />
+      <CreateAppRole
+        x-if={modal.scene === 'create'}
+        open={modal.open}
+        title={modal.title}
+        id={modal.id}
+        appId={appInfo?.id}
+        onClose={onDrawerClose}
+      />
+      <DrawerRolePolicy
+        x-if={modal.scene === 'addPolicy' && modal.open}
+        open={modal.open}
+        title={modal.title}
+        appInfo={appInfo}
+        roleInfo={modal.roleInfo}
+        onClose={onDrawerClose}
+      />
+    </PageContainer>
   );
+};
+
+export default () => {
+  return <KeepAlive>
+    <AppRoleList />
+  </KeepAlive>
 };
